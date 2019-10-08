4a875a18d2d1df9319dae542a31f40965fdccbd4
raulojeda22 <raulojeda10g@gmail.com>
2019-10-08 20:29:16 +0200

    Graphql for products, hotels and rooms

--- backend/graphql/resolvers/resolver.js
+++ backend/graphql/resolvers/resolver.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const Restaurant = mongoose.model('Restaurant');
+const Product = mongoose.model('Product');
+const Hotel = mongoose.model('Hotel');
+const Room = mongoose.model('Room');
+
+
 
 const resolvers = {
   Query: {
@@ -8,6 +13,24 @@ const resolvers = {
       },
       restaurants: () => {
           return Restaurant.find();
+      },
+      product: (root, {slug}) => {
+        return Product.findOne({slug: slug});
+      },
+      products: () =>  {
+        return Product.find();
+      },
+      hotel: (root, {slug}) => {
+        return Hotel.findOne({slug: slug});
+      },
+      hotels: () => {
+          return Hotel.find();
+      },
+      room: (root, {slug}) => {
+          return Room.findOne({slug: slug});
+      },
+      rooms: () => {
+          return Room.find();
       },
       message: () => 'Hello World!'
   }
--- backend/graphql/schemas/schema.js
+++ backend/graphql/schemas/schema.js
@@ -6,6 +6,12 @@ const typeDefs = gql`
         message: String
         restaurant(slug: String!): Restaurant
         restaurants: [Restaurant]
+        product(slug: String!): Product
+        products: [Product]
+        hotel(slug: String!): Hotel
+        hotels: [Hotel]
+        room(slug: String!): Room
+        rooms: [Room]
     }
     type Restaurant {
         id: ID!
@@ -15,6 +21,35 @@ const typeDefs = gql`
         price: Int
         createdAt: Date
         updatedAt: Date
+    }
+    type Product {
+        id: ID!
+        slug: String!
+        title: String
+        description: String
+        price: Int
+        createdAt: Date
+        updatedAt: Date
+    }
+    type Hotel {
+        id: ID!
+        slug: String!
+        name: String
+        description: String
+        city: String
+        inDate: String
+        outDate: String
+        stars: Int
+        reviewScore: Int
+        features: [String]
+        rooms: Int
+        services: String
+    }
+    type Room {
+        id: ID!
+        beds: Int
+        equipment: [String]
+        occupied: Boolean
     }
 `;
 

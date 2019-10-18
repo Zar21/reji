253a9a2189ead525b346366e6efdc4ac0e2c1e24
Ethan Hildick <hildickethan@gmail.com>
2019-10-18 17:15:15 +0200

    Added GQL frontend reference link in it's service

--- frontend/src/js/services/graphql.service.js
+++ frontend/src/js/services/graphql.service.js
@@ -2,7 +2,7 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import gql from 'graphql-tag'
-
+// https://medium.com/free-code-camp/graphql-front-end-queries-made-easy-68e9d9ded283
 export default class GraphQL {
     constructor(AppConstants, $q) {
         'ngInject';

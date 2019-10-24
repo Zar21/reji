dbfab4c21b302edc951a21df4f20a6e3ad259770
jordilg13 <jordillopis00@gmail.com>
2019-10-24 18:29:38 +0200

    entrypoint to migrate graphql to a new backend

---
+++ backend/backend_rest/clearCollections.js
@@ -0,0 +1,8 @@
+use conduit_nodejs
+db.hotels.drop()
+db.travels.drop()
+db.rooms.drop()
+db.adventures.drop()
+db.cities.drop()
+db.countries.drop()
+db.products.drop()
---
+++ backend/backend_rest/routes/api/index.js
@@ -0,0 +1,31 @@
+var router = require('express').Router();
+import SERVER from "./graphql";
+
+router.use('/', require('./users'));
+router.use('/profiles', require('./profiles'));
+router.use('/articles', require('./articles'));
+router.use('/tags', require('./tags'));
+router.use('/contact', require('./contact'));
+router.use('/adventures', require('./adventures/adventures'));
+router.use('/hotels', require('./hotels/hotels'));
+router.use('/cities', require('./travels/cities'));
+router.use('/countries', require('./travels/countries'));
+router.use('/travels', require('./travels/travels'));
+router.use('/restaurants', require('./restaurants/restaurants'));
+SERVER.applyMiddleware({ app: router, path:'/graphql' });
+
+router.use(function(err, req, res, next){
+  if(err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key){
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {})
+    });
+  }
+
+  return next(err);
+});
+
+module.exports = router;
